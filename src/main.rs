e47fb25f7fdcbdf4841b0af92b5fbc343edc3761
Sonny To <son.c.to@gmail.com>
2023-05-22 11:13:20 -0500

    compiles

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,74 @@ use winit::{
     window::WindowBuilder,
 };
 
+use ash::extensions::ext::DebugUtils;
+use ash::{vk, Entry, Instance};
+
+use raw_window_handle::HasRawDisplayHandle;
+use std::ffi::CStr;
+use std::os::raw::c_char;
+
 #[allow(dead_code)]
 struct VulkanApp {
     event_loop: EventLoop<()>,
     window: winit::window::Window,
+    entry: Entry,
+    instance: Instance,
 }
 
 impl VulkanApp {
     fn new() -> VulkanApp {
-        let event_loop = EventLoop::new();
-        let window = WindowBuilder::new()
-            .with_title("Triangle Foobar")
-            .build(&event_loop)
-            .unwrap();
-        VulkanApp { event_loop, window }
+        unsafe {
+            let event_loop = EventLoop::new();
+            let window = WindowBuilder::new()
+                .with_title("Triangle Foobar")
+                .build(&event_loop)
+                .unwrap();
+            let entry = Entry::linked();
+            let app_name = CStr::from_bytes_with_nul_unchecked(b"VulkanTriangle\0");
+
+            let appinfo = vk::ApplicationInfo::builder()
+                .application_name(app_name)
+                .application_version(0)
+                .engine_name(app_name)
+                .engine_version(0)
+                .api_version(vk::make_api_version(0, 1, 0, 0));
+
+            let layer_names = [CStr::from_bytes_with_nul_unchecked(
+                b"VK_LAYER_KHRONOS_validation\0",
+            )];
+            let layers_names_raw: Vec<*const c_char> = layer_names
+                .iter()
+                .map(|raw_name| raw_name.as_ptr())
+                .collect();
+            let mut extension_names =
+                ash_window::enumerate_required_extensions(window.raw_display_handle())
+                    .unwrap()
+                    .to_vec();
+            extension_names.push(DebugUtils::name().as_ptr());
+
+            let create_flags = if cfg!(any(target_os = "macos", target_os = "ios")) {
+                vk::InstanceCreateFlags::ENUMERATE_PORTABILITY_KHR
+            } else {
+                vk::InstanceCreateFlags::default()
+            };
+
+            let create_info = vk::InstanceCreateInfo::builder()
+                .application_info(&appinfo)
+                .enabled_layer_names(&layers_names_raw)
+                .enabled_extension_names(&extension_names)
+                .flags(create_flags);
+
+            let instance: Instance = entry
+                .create_instance(&create_info, None)
+                .expect("Instance creation error");
+            VulkanApp {
+                event_loop,
+                window,
+                entry,
+                instance,
+            }
+        }
     }
 
     fn run(self) {
@@ -33,7 +87,7 @@ impl VulkanApp {
                         println!("input.virtual_keycode={:?}", input.virtual_keycode);
                         if let Some(VirtualKeyCode::Escape) = input.virtual_keycode {
                             if input.state == ElementState::Pressed {
-				*control_flow = ControlFlow::Exit;
+                                *control_flow = ControlFlow::Exit;
                                 println!("Escape key pressed!");
                             } else if input.state == ElementState::Released {
                                 println!("Escape key released!");
